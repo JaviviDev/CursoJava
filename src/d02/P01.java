0963b0d2261ae691e3c077b3276400812a18295c
javieralocue <javier.alocue@educa.jcyl.es>
2025-02-06 12:23:48 +0100

    P01 hecho

--- src/d02/P01.java
+++ src/d02/P01.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class P01 {
     public static void main(String[] args) {
         Scanner entrada= new Scanner(System.in);
-        System.out.print("Escribe tu número");
+        System.out.print("Escribe tu número ");
         int n1=entrada.nextInt();
-        System.out.println("Tu número es "+n1);
+        System.out.print("Escribe otro número");
+        int n2=entrada.nextInt();
+        System.out.println("Es 1er número es "+n1+" y el otro "+n2);
+        int suma=n1+n2;
+        System.out.println("La suma es "+suma);
     }
 }
